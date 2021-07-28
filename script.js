459d48c3dd5bbf2fbd5157c4ca482170713bc8b9
Alex-Borisenko77 <zzcarrythzz@gmail.com>
2021-07-28 13:25:12 +0300

    completed task

--- script.js
+++ script.js
@@ -29,90 +29,78 @@ function sideShift2(arr, n) {
 
 //third task
 
-// const arrOfStr = ['abbasqwer', ' dsadaqwer']
-// const str1 = 'dsadsaqwerqqq';
-// const str2 = 'dsdaaqwerba'
-// const finder = (line1, line2) => {
-//     let arr = [];
-//     line1.split('').reduce((last, item) => {
-//         if (line2.indexOf(`${last}${item}`) !== -1) {
-//             arr.push(`${last}${item}`);
-//             return `${last}${item}`;
-//         } else return item;
-//     }, '');
-//     return arr.sort((a, b) => b.length - a.length)[0]
-// }
 
-
-
+const str1 = 'dsandsaqwerqbananaoobaskgoal';
+const str2 = 'dsdanaqwerkkkqwwbananaqqq'
+const finder = (line1, line2) => {
+    let arr = [];
+    line1.split('').reduce((last, item) => {
+        if (line2.indexOf(`${last}${item}`) !== -1) {
+            arr.push(`${last}${item}`);
+            return `${last}${item}`;
+        } else return item;
+    });
+    return arr.sort((a, b) => b.length - a.length)[0]
+}
 
 
 //Fourth task
-const showAll = document.querySelector('.checkboxAll');
-const showNew = document.querySelector('.checkboxNew');
-const showHot = document.querySelector('.checkboxHot');
-const showBest = document.querySelector('.checkboxBest');
 const allNews = document.querySelectorAll('.main__content-items')
 const newInfo = document.querySelectorAll('.new');
 const hotInfo = document.querySelectorAll('.hot');
 const bestInfo = document.querySelectorAll('.best');
-
-showAll.addEventListener('click', (e) => {
-    if (showAll.checked) {
-        showNew.checked = false;
-        showBest.checked = false;
-        showHot.checked = false;
+const allCheckbox = document.querySelectorAll('.check')
+const eachAllHandler = (e) => {
+    allCheckbox.forEach((item) => {
         allNews.forEach((elem) => elem.style.display = 'block')
-    }
-    if (!showAll.checked) {
-        allNews.forEach((elem) => elem.style.display = 'none')
-    }
+        if (item.id !== e) {
+            item.checked = false
+        }
+    })
+}
+const eachNewHandler = (e) => {
+    allCheckbox.forEach((item) => {
+        hotInfo.forEach(({style}) => style.display = 'none')
+        bestInfo.forEach(({style}) => style.display = 'none')
+        newInfo.forEach(({style}) => style.display = 'block')
+        if (item.id !== e) {
+            item.checked = false
+        }
+    })
+}
+const eachHotHandler = (e) => {
+    newInfo.forEach((elem) => elem.style.display = 'none')
+    bestInfo.forEach((elem) => elem.style.display = 'none')
+    hotInfo.forEach((elem) => elem.style.display = 'block')
+    allCheckbox.forEach((item) => {
+        if (item.id !== e) {
+            item.checked = false
+        }
+    })
+}
+const eachBestHandler = (e) => {
+    newInfo.forEach((elem) => elem.style.display = 'none')
+    hotInfo.forEach((elem) => elem.style.display = 'none')
+    bestInfo.forEach((elem) => elem.style.display = 'block')
+    return allCheckbox.forEach((item) => {
+        if (item.id !== e) {
+            item.checked = false
+        }
+    })
+}
+allCheckbox.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case 'All':
+                return eachAllHandler(e.target.id)
+            case 'new':
+                return eachNewHandler(e.target.id)
+            case 'hot':
+                return eachHotHandler(e.target.id)
+            case 'best':
+                return eachBestHandler(e.target.id)
+            default:
+                return null
+        }
+    })
 })
-
-
-showNew.addEventListener('click', (e) => {
-
-    if (showNew.checked) {
-        showAll.checked = false;
-        showBest.checked = false;
-        showHot.checked = false
-        hotInfo.forEach((elem) => elem.style.display = 'none')
-        bestInfo.forEach((elem) => elem.style.display = 'none')
-        newInfo.forEach((elem) => elem.style.display = 'block')
-    }
-    if (!showAll.checked) {
-        newInfo.forEach((elem) => elem.style.display = 'none')
-    }
-})
-
-
-showHot.addEventListener('click', (e) => {
-    if (showHot.checked) {
-        showAll.checked = false;
-        showNew.checked = false;
-        showBest.checked = false;
-        newInfo.forEach((elem) => elem.style.display = 'none')
-        bestInfo.forEach((elem) => elem.style.display = 'none')
-        hotInfo.forEach((elem) => elem.style.display = 'block')
-    }
-    if (showHot.checked === false) {
-        hotInfo.forEach((elem) => elem.style.display = 'none')
-    }
-})
-
-
-showBest.addEventListener('click', (e) => {
-    if (showBest.checked) {
-        showAll.checked = false;
-        showNew.checked = false;
-        showHot.checked = false;
-        newInfo.forEach((elem) => elem.style.display = 'none')
-        hotInfo.forEach((elem) => elem.style.display = 'none')
-        bestInfo.forEach((elem) => elem.style.display = 'block')
-    }
-    if (showBest.checked === false) {
-        bestInfo.forEach((elem) => elem.style.display = 'none')
-    }
-})
-
-
